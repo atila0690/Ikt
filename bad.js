fd07825217f00d2d6e6e74acab0c3897fa9824c5
atila0690 <tothattila0618@gmail.com>
2023-05-03 16:11:34 +0200

    0916

--- bad.js
+++ bad.js
@@ -1,17 +1,29 @@
 const products = [
     {
       name: "Product 1",
-      price: 19.99,
-      image: "product1.jpg"
+      price: 10000,
+      image: "https://cdn.discordapp.com/attachments/1088384919157350481/1103031398320840854/pelda.png"
     },
     {
       name: "Product 2",
-      price: 29.99,
-      image: "product2.jpg"
+      price: 20000,
+      image: "https://cdn.discordapp.com/attachments/1088384919157350481/1103031398320840854/pelda.png"
     },
     {
       name: "Product 3",
-      price: 39.99,
-      image: "product3.jpg"
+      price: 30000,
+      image: "https://cdn.discordapp.com/attachments/1088384919157350481/1103031398320840854/pelda.png"
     }
   ];
+
+
+const link = document.getElementById("link");
+const image = document.getElementById("image");
+
+link.addEventListener("click", function() {
+  window.location.href = "Shop.html";
+});
+
+image.addEventListener("click", function(event) {
+  event.stopPropagation();
+});
