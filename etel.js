2d70051b2bbebb3242f92cf84d9a32e1b82d2b3e
zomborr <120582010+zomborr@users.noreply.github.com>
2023-05-10 16:07:12 +0200

    Update etel.js

--- etel.js
+++ etel.js
@@ -4,3 +4,9 @@ const products = [
       name: "Product 1",
       price: 2400,
       image: "https://behir.hu/web/content/media/2020/08/bundas_kenyer-800x450.jpg"
+},
+    {
+      name: "Product 2",
+      price: 1800,
+      image: "https://www.glutenmentesetrend.hu/wp-content/uploads/vaddisznoporkolt-glutenmentes-galuskaval-920x425.jpg"
+    },
